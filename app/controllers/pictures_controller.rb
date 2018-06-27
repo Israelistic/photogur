b47c899d331b8c7456440b9a70c2e6b97655cf58
Israelistic <haguyle@gmail.com>
2018-06-26 20:29:41 -0400

    asure that when picture is created its saved with the current user id

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,10 @@
 class PicturesController < ApplicationController
     before_action :ensure_logged_in, except: [:show, :index]
-# run the  mentiond method before executing the code 
+      # before_action :new_picture, only: [:new, :create]
+    # before_action :load_picture, only: [:show, :edit, :update, :destroy]
+    # before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
+
+# run the  mentiond method before executing the code
     def index
         @most_recent_pictures = Picture.most_recent_five
         @pictures_in_year = Picture.pictures_created_in_year(Time.now.year)
@@ -20,6 +24,8 @@ class PicturesController < ApplicationController
          @picture.title = params[:picture][:title]
          @picture.artist = params[:picture][:artist]
          @picture.url = params[:picture][:url]
+         @picture.user = current_user
+         # User.find(session[:user_id]) session[:user_id]
          if @picture.save
            # if the picture gets saved, generate a get request to "/pictures" (the index)
            redirect_to "/pictures"
@@ -47,4 +53,13 @@ class PicturesController < ApplicationController
         @picture.destroy
         redirect_to "/pictures"
     end
+    # def ensure_user_owns_picture
+    #   if current_user == nil
+    #     flash[:alert] = "Please log in."
+    #     redirect_to new_sessions_path
+    #   elsif current_user != @picture.user
+    #     flash[:alert] = "Sorry, you do not have access to this picture."
+    #     redirect_to picture_path
+    #   end
+    # end
 end
